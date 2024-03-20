3f66a6d765ced3ec6bdecf52823f649af73ef35b
DeenoDev <deyanoklaasen@outlook.com>
2024-03-20 21:50:17 +0200

    imported state, actions, mutations and getters from respective file into store/index.js file.

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,8 @@
 import {createStore} from 'vuex';
+import state from './state';
+import actions from './actions';
+import mutations from './mutations';
+import getters from './getters';
 
 const store = createStore({
     state,
