3afe4ef15d911e929f742660a35a34a6007b98c8
DeenoDev <deyanoklaasen@outlook.com>
2024-03-21 22:24:53 +0200

    tweaked positioning of alphabet beneath the search bar.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,8 @@
 <template>
-    <div class="flex p-8 justify-center">
+    <div class="flex flex-col p-8">
         <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for Meals">
 
         <div class="flex gap-1">
-            <!-- <pre>{{ letters }}</pre> -->
             <router-link to="/" v-for="letter of letters">
                 {{ letter }}
 
