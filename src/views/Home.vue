a66ad3065a4a9ed2f13d880c8ddbf5d9282b4ac1
DeenoDev <deyanoklaasen@outlook.com>
2024-03-21 22:21:40 +0200

    Added alphabet to Home.vue component.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="flex p-8 justify-center">
         <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for Meals">
+
+        <div class="flex gap-1">
+            <!-- <pre>{{ letters }}</pre> -->
+            <router-link to="/" v-for="letter of letters">
+                {{ letter }}
+
+
+            </router-link>
+
+
+        </div>
     </div>
 </template>
 
@@ -10,5 +21,6 @@ import store from '../store';
 
 
 const meals = computed(() => store.state.meals);
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
 
 </script>
