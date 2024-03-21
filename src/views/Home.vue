6ec290270a4054d1a751d41d79a26742c0d95587
DeenoDev <deyanoklaasen@outlook.com>
2024-03-21 23:34:29 +0200

    ingredients JSON displayed in Home.vue component for testing API.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,27 +5,28 @@
         <div class="flex gap-4 justify-center mt-2">
             <router-link :to='{name: "byLetter", params: { letter }}' v-for="letter of letters" :key ="letter">
                 {{ letter }}
-
-
             </router-link>
-
-
         </div>
+
+        <pre>{{ ingredients }}</pre>
     </div>
 </template>
 
 <script setup>
-import { computed, onMounted } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import store from '../store';
 import axiosClient from '../axiosClient.js';
 
 
 
+
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
+const ingredients = ref([]);
 
 onMounted( async() => {
   const response = await axiosClient.get('/list.php?i=list');
   console.log(response.data);
+  ingredients.value = response.data;
 });
 
 </script>
