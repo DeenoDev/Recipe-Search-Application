244956790ca176a260e3ebfffb8eb7c0871b5dc3
DeenoDev <deyanoklaasen@outlook.com>
2024-03-21 23:51:15 +0200

    Added h-full style to app.js in index.js and reduced header height to h-16 from h-24.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 
 <template>
-  <div class="bg-gray-100">
-    <header class="bg-white shadow-md h-24">
+  <div class="bg-gray-100 h-full">
+    <header class="bg-white shadow-md h-16">
 
     </header>
     <main>
