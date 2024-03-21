d70b36621a2484eb88039d18aac90521fb92c1cb
DeenoDev <deyanoklaasen@outlook.com>
2024-03-21 22:13:30 +0200

    completed input styling for search bar.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex p-8 justify-center">
-        <input type="text" class="rounded border-2 border-gray-200">
+        <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for Meals">
     </div>
 </template>
 
