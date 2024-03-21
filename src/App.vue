ac3cbd5100bc759d6110cf4ec60045825c4a93e5
DeenoDev <deyanoklaasen@outlook.com>
2024-03-21 23:37:47 +0200

    Started creating page layout by adding tags and styling in App.vue component template.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 
 <template>
-  <router-view />
+  <div class="bg-gray-100">
+    <header class="bg-white shadow-md h-24">
+
+    </header>
+    <main>
+      <router-view />
+    </main>
+  </div>
   
 </template>
 
