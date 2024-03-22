c9bffadece94513cdf41f34e57ca06942cf5329c
DeenoDev <deyanoklaasen@outlook.com>
2024-03-22 23:03:41 +0200

    Added hover effects and transition on colors for router link navigation.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 
 <template>
   <div class="bg-gray-100 h-full">
-    <header class="bg-white shadow h-16 flex justify-between items-center">
+    <header class="bg-white shadow h-16 flex justify-between items-stretch">
       <router-link :to="{name: 'home'}"> 
         Home
       </router-link>
       <!-- try using UL tag for this section when tutorial is completed -->
-      <div>
-      <router-link to="/" class="px-2 h-full hover:bg-purple-200">
-        Search Meals
-        </router-link>
-        <router-link to="/" class="px-2 h-full hover:bg-purple-200">
+      <div class="flex items-center gap-1">
+       <router-link to="/" class="inline-flex items center px-2 h-full transition-colors hover:bg-purple-300 pt-5">
+         Search Meals
+         </router-link>
+         <router-link to="/" class="inline-flex px-2 center h-full transition-colors hover:bg-purple-300 pt-5">
           Meals By Letter
-        </router-link>
-        <router-link to="/" class="px-2 h-full hover:bg-purple-200">
+         </router-link>
+         <router-link to="/" class="inline-flex px-2 center h-full transition-colors hover:bg-purple-300 pt-5">
           Meals By Ingredients
-        </router-link>
+         </router-link>
 
       </div>
 
