d1f8da53688a358154517b870a86198e985b1eba
DeenoDev <deyanoklaasen@outlook.com>
2024-03-22 22:54:17 +0200

    Added basic styling to navigation.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 
 <template>
   <div class="bg-gray-100 h-full">
-    <header class="bg-white shadow h-16">
+    <header class="bg-white shadow h-16 flex justify-between items-center">
       <router-link :to="{name: 'home'}"> 
         Home
       </router-link>
       <!-- try using UL tag for this section when tutorial is completed -->
       <div>
-        <router-link to="/">
+      <router-link to="/" class="px-2 h-full hover:bg-purple-200">
         Search Meals
         </router-link>
-        <router-link to="/">
+        <router-link to="/" class="px-2 h-full hover:bg-purple-200">
           Meals By Letter
         </router-link>
-        <router-link to="/">
+        <router-link to="/" class="px-2 h-full hover:bg-purple-200">
           Meals By Ingredients
         </router-link>
 
