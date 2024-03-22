8be587d106f2bd2bd4b4197d1e3444d034e88b1e
DeenoDev <deyanoklaasen@outlook.com>
2024-03-22 23:43:27 +0200

    Added path to router link for search options in App.vue.

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,13 @@
       </router-link>
       <!-- try using UL tag for this section when tutorial is completed -->
       <div class="flex items-center gap-1">
-       <router-link to="/" class="inline-flex items center px-2 h-full transition-colors hover:bg-purple-300 pt-5">
+       <router-link :to="{name: 'byName'}" class="inline-flex items center px-2 h-full transition-colors hover:bg-purple-300 pt-5">
          Search Meals
          </router-link>
-         <router-link to="/" class="inline-flex px-2 center h-full transition-colors hover:bg-purple-300 pt-5">
+         <router-link :to="{name: 'byLetter'}" class="inline-flex px-2 center h-full transition-colors hover:bg-purple-300 pt-5">
           Meals By Letter
          </router-link>
-         <router-link to="/" class="inline-flex px-2 center h-full transition-colors hover:bg-purple-300 pt-5">
+         <router-link :to="{name: 'byIngredients'}" class="inline-flex px-2 center h-full transition-colors hover:bg-purple-300 pt-5">
           Meals By Ingredients
          </router-link>
 
