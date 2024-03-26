ca24b6e8408a20b6325349b720f87499b7309b04
DeenoDev <deyanoklaasen@outlook.com>
2024-03-26 23:59:26 +0200

    Added padding to seach bar on mealsbyname component.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col p-8">
-        <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for Meals">
+        
 
         <div class="flex gap-4 justify-center mt-2">
             <router-link :to='{name: "byLetter", params: { letter }}' v-for="letter of letters" :key ="letter">
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="p-8">
         <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for Meals">
     </div>
 
