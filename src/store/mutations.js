7dbcea1033d298c42b5f65b0765a0b96cb1fa853
DeenoDev <deyanoklaasen@outlook.com>
2024-03-28 00:08:29 +0200

    store bug needs to be fixed in next session.

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,3 +1,3 @@
-export default setSearchedMeals(state, meals) {
-    state.searchedMeals = meals
+export function setSearchedMeals(state, meals) {
+    state.searchedMeals = meals;
 }
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -4,7 +4,7 @@
         v-model="keyword"
         class="rounded border-2 border-gray-200 w-full" 
         placeholder="Search for Meals"
-        @change="searchMeals">
+        @change="searchMeals()">
         
     </div>
 
