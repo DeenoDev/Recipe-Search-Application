1b2bb1028bc8357ef3c627d7090d690835d9d71d
DeenoDev <deyanoklaasen@outlook.com>
2024-03-29 23:57:45 +0200

    Added additional styling to meals display.

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -9,11 +9,11 @@
        />   
     </div>
 
-    <div>
-        <div v-for="meal of meals" :key="meal.idMeal" class="grid grid-cols-1 md:grid-cols-3">
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+        <div v-for="meal of meals" :key="meal.idMeal" class="bg-white shadow rounded">
         <img :src="meal.strMealThumb" alt="strMeal">
-        <h3>{{ meal.strMeal }}</h3>
-        <div>
+        <h3 class="px-3 py-3 font-semibold">{{ meal.strMeal }}</h3>
+        <div class="px-3 py-3">
           <a :href="meal.strYoutube" target="_blank">Youtube</a>
           <router-link to="/">View</router-link>
         </div>
