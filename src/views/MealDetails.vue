99b418fa001f9e12ac1cce11a0c5e914bc431991
DeenoDev <deyanoklaasen@outlook.com>
2024-04-04 00:01:14 +0200

    added basic styling to h2 heading in Ingredients section.

--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -21,7 +21,7 @@
 
         <div class="grid grid-cols-1 md:grid-cols2">
             <div>
-                <h2 class="text-2xl">Ingredients</h2>
+                <h2 class="text-2xl font-semibold mb-3">Ingredients</h2>
                 
             </div>
 
