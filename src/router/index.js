08f06ce8f0a6835b6fc75907e1a5d2b10d3ceac6
DeenoDev <deyanoklaasen@outlook.com>
2024-04-03 22:40:17 +0200

    router bug fixed due to typo.

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ import Home from '../views/Home.vue';
 import MealsByIngredient from '../views/MealsByIngredient.vue';
 import MealsByLetter from '../views/MealsByLetter.vue';
 import MealsByName from '../views/MealsByName.vue';
-import MealsByName from '../views/MealDetails.vue';
+import MealDetails from '../views/MealDetails.vue';
 
 
 const routes = [
