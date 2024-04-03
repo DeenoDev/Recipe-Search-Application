d4b5e4a3bf042f81b8287c047ba9c478ebcaad74
DeenoDev <deyanoklaasen@outlook.com>
2024-04-03 23:32:45 +0200

    Added path to router link.

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -13,7 +13,7 @@
         <div v-for="meal of meals" :key="meal.idMeal" class="bg-white shadow rounded-xl">
         <router-link>
           <img :src="meal.strMealThumb" alt="strMeal" class="rounded-t-xl w-full h-48 object-cover">
-        </router-link to="/">
+        </router-link to="{name: 'mealDetails', params: {id: meal.idMeal}}">
         <div class="p-3 ">
           <h3 class="font-bold">{{ meal.strMeal }}</h3>
           <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro dicta vitae, illo consectetur adipisci numquam</p>
