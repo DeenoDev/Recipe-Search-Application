9a86e4d152a48a6338da7636404ccf94cb821ec6
DeenoDev <deyanoklaasen@outlook.com>
2024-04-08 23:53:23 +0200

    moved youtube button script from MealsByName Component to YouTubeButton.vue component.

--- src/components/YouTubeButton.vue
+++ src/components/YouTubeButton.vue
@@ -1,8 +1,12 @@
 <template>
-
+    <a :href="meal.strYoutube" target="_blank" class="px-3 py-2 rounded border-2 border-red-600 bg-red-500 hover:bg-red-600 text-white transition-colors"> Youtube </a>
 
 </template>
 
-<script>
+<script setup>
+
+const props = defineProps({
+    href: String
+});
 
 </script>
