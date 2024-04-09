5048b70d8f3ddc0f8fc8490fb998a8b1d66fc808
DeenoDev <deyanoklaasen@outlook.com>
2024-04-09 23:43:19 +0200

    Used slot to render youtube button in MealDetails.vue file.

--- src/components/YouTubeButton.vue
+++ src/components/YouTubeButton.vue
@@ -1,5 +1,8 @@
 <template>
-    <a :href="href" target="_blank" class="px-3 py-2 rounded border-2 border-red-600 bg-red-500 hover:bg-red-600 text-white transition-colors"> Youtube </a>
+    <a :href="href" target="_blank" class="px-3 py-2 rounded border-2 border-red-600 bg-red-500 hover:bg-red-600 text-white transition-colors"> Youtube 
+    
+        <slot></slot>
+    </a>
 
 </template>
 
