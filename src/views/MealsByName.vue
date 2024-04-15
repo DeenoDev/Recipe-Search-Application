49e0e75d9488bf4fe88678420c47622652aba2ba
DeenoDev <deyanoklaasen@outlook.com>
2024-04-15 23:54:53 +0200

    rendered MealItem component with v-for directive inside MealsByName component.

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -10,21 +10,7 @@
     </div>
 
     <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-        <div v-for="meal of meals" :key="meal.idMeal" class="bg-white shadow rounded-xl">
-        <router-link :to="{name: 'mealDetails', params: {id: meal.idMeal}}">
-          <img :src="meal.strMealThumb" alt="strMeal" class="rounded-t-xl w-full h-48 object-cover">
-        </router-link>
-        <div class="p-3 ">
-          <h3 class="font-bold">{{ meal.strMeal }}</h3>
-          <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro dicta vitae, illo consectetur adipisci numquam</p>
-          <div class="flex items-center justify-between">
-           <YouTubeButton :href="meal.strYoutube">
-          Youtube
-          </YouTubeButton>
-            <!-- <router-link to="/" class="px-3 py-2 rounded border-2 border-purple-600 bg-purple-500 hover:bg-purple-600 text-white transition-colors"> View</router-link> -->
-        </div>
-        </div>
-        </div>
+       <MealItem v-for="meal of meals" />
     </div>
 
 </template>
@@ -35,6 +21,7 @@ import { onMounted, ref } from 'vue';
 import store from '../store';
 import { useRoute } from 'vue-router';
 import YouTubeButton from '../components/YouTubeButton.vue';
+import MealItem from '../components/MealItem.vue';
 
 const route = useRoute();
 
