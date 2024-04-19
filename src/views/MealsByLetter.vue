f792c8e84f2e98aea2b2fc6db67f42f2c7d4976f
DeenoDev <deyanoklaasen@outlook.com>
2024-04-20 00:04:45 +0200

    Added router-link to MealsByLetter.vue component.

--- src/views/MealsByLetter.vue
+++ src/views/MealsByLetter.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
-        By Letter
+        <div class="flex flex-wrap justify-center gap-3 px-8 mb-6">
+    <router-link
+      :to="{ name: 'byLetter', params: { letter } }"
+      v-for="letter of letters"
+      :key="letter"
+      class="w-2 h-2 flex items-center justify-center hover:text-orange-500 hover:scale-150 transition-all"
+    >
+      {{ letter }}
+    </router-link>
+        </div>
     </div>
 
 </template>
