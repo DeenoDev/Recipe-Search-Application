f412f7b371cfc03da868f8e4d8ef7387c7599024
DeenoDev <deyanoklaasen@outlook.com>
2024-04-24 23:20:15 +0200

    completed onMounted script for displaying ingredients.

--- src/views/MealsByIngredient.vue
+++ src/views/MealsByIngredient.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        By Ingredients
+        <pre>{{ ingredients }}</pre>
     </div>
 
 </template>
@@ -9,9 +9,15 @@
 import { onMounted } from 'vue';
 import axiosClient from '../axiosClient';
 
+const ingredients = ref([]);
+
 
 onMounted(() => {
     axiosClient.get('list.php?=list')
+    .then(({data}) => {
+        debugger;
+        ingredients.value = data;
+    })
 })
 
 </script>
