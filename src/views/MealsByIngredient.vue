46d2f85fe368972481144b2cd2e7209cf3df174b
DeenoDev <deyanoklaasen@outlook.com>
2024-04-25 23:48:38 +0200

    added router-link for ingredient div.

--- src/views/MealsByIngredient.vue
+++ src/views/MealsByIngredient.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="p-8">
         <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
-        <div v-for="ingredient of ingredients" :key="ingredient.id" 
-        class="bg-white rounded p-3 mb-3 shadow">
+        <router-link v-for="ingredient of ingredients" :key="ingredient.id" 
+          class="bg-white rounded p-3 mb-3 shadow">
           <h3 class="text2xl font-bold mb-2"> {{ ingredient.strIngredient }} </h3>
           <p> {{ ingredient.strDescription }} </p>
-        </div>
+        </router-link>
     </div>
 
 </template>
