1e855a762ca94fbc6f834c3115da0bfdacc96cd6
DeenoDev <deyanoklaasen@outlook.com>
2024-04-25 23:45:40 +0200

    Added basic styling to h1 tag in ingredients section.

--- src/views/MealsByIngredient.vue
+++ src/views/MealsByIngredient.vue
@@ -1,8 +1,9 @@
 <template>
-    <div>
+    <div class="p-8">
+        <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
         <div v-for="ingredient of ingredients" :key="ingredient.id" 
         class="bg-white rounded p-3 mb-3 shadow">
-          <h3 class="text2xl font-bold"> {{ ingredient.strIngredient }} </h3>
+          <h3 class="text2xl font-bold mb-2"> {{ ingredient.strIngredient }} </h3>
           <p> {{ ingredient.strDescription }} </p>
         </div>
     </div>
