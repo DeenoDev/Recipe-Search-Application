25bb60d01f191f98f1f2e95423a6e21ef70137a8
DeenoDev <deyanoklaasen@outlook.com>
2024-04-27 23:47:40 +0200

    created Ingredients.vue component in views folder.

---
+++ src/views/Ingredients.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="p-8">
+        <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
+        <router-link :to="{name: 'byIngredient', params: {ingredient: ingredient.idIngredient}}"  v-for="ingredient of ingredients" :key="ingredient.id" 
+          class="bg-white rounded p-3 mb-3 shadow">
+          <h3 class="block text2xl font-bold mb-2"> {{ ingredient.strIngredient }} </h3>
+          <p> {{ ingredient.strDescription }} </p>
+        </router-link>
+    </div>
+
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import axiosClient from '../axiosClient';
+
+const ingredients = ref([]);
+
+
+onMounted(() => {
+    axiosClient.get('list.php?=list')
+    .then(({data}) => {
+        debugger;
+        ingredients.value = data.meal;
+    })
+})
+
+</script>
