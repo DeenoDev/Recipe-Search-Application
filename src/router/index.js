fcb56e408dcbfa356e7d0b4c9f471cc2770e87b0
DeenoDev <deyanoklaasen@outlook.com>
2024-04-28 23:44:46 +0200

    removed ? from ingredient path route to make request mandatory.

--- src/router/index.js
+++ src/router/index.js
@@ -35,7 +35,7 @@ const routes = [
                 component: Ingredients,
             },
             {
-                path: "/by-ingredient/:ingredient?",
+                path: "/by-ingredient/:ingredient",
                 name: "byIngredient",
                 component: MealsByIngredient,
             },
