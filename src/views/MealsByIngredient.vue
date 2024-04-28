28bc074d8c95a05905110f997c91a177afe71159
DeenoDev <deyanoklaasen@outlook.com>
2024-04-28 23:31:11 +0200

    replaced meals by ingredient template with MealItem component.

--- src/views/MealsByIngredient.vue
+++ src/views/MealsByIngredient.vue
@@ -1,12 +1,7 @@
 <template>
-    <div class="p-8">
-        <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
-        <router-link :to="{name: 'byIngredient', params: {ingredient: ingredient.idIngredient}}"  v-for="ingredient of ingredients" :key="ingredient.id" 
-          class="bg-white rounded p-3 mb-3 shadow">
-          <h3 class="block text2xl font-bold mb-2"> {{ ingredient.strIngredient }} </h3>
-          <p> {{ ingredient.strDescription }} </p>
-        </router-link>
-    </div>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+    <MealItem v-for="meal of meals" :key="meal.idMeal" :meals="meals" />
+  </div>
 
 </template>
 
