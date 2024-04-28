980593d3fee5ffa8337ed85d3148c2733c139687
DeenoDev <deyanoklaasen@outlook.com>
2024-04-28 23:36:06 +0200

    removed script for MealsByIngredients to replace with script for functionality in MealItem component.

--- src/views/MealsByIngredient.vue
+++ src/views/MealsByIngredient.vue
@@ -9,15 +9,6 @@
 import { onMounted } from 'vue';
 import axiosClient from '../axiosClient';
 
-const ingredients = ref([]);
 
-
-onMounted(() => {
-    axiosClient.get('list.php?=list')
-    .then(({data}) => {
-        debugger;
-        ingredients.value = data.meal;
-    })
-})
 
 </script>
