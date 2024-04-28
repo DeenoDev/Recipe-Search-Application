8e26e58db763da2775a133804b5809a0068b8c04
DeenoDev <deyanoklaasen@outlook.com>
2024-04-28 23:42:14 +0200

    created onMounted function for route.

--- src/views/MealsByIngredient.vue
+++ src/views/MealsByIngredient.vue
@@ -9,7 +9,13 @@
 import { computed, onMounted } from 'vue';
 import store from '../store';
 
+
+const route = useRoute();
 const meals = computed(() => store.state.mealsByIngredient);
+
+onMounted(() => {
+    store.dispatch('searchedMealsByIngredient', route.params.ingredient )
+});
 
 
 
