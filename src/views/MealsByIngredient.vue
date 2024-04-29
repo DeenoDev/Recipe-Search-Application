856253e92aa0c92e82ada145b8ed8a59c7cf0fcf
DeenoDev <deyanoklaasen@outlook.com>
2024-04-29 23:55:46 +0200

    plugged Meals.vue component into MealsByIngredient.

--- src/views/MealsByIngredient.vue
+++ src/views/MealsByIngredient.vue
@@ -1,10 +1,5 @@
 <template>
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-    <MealItem v-for="meal of meals" :key="meal.idMeal" :meals="meals" />
-  </div>
-  <div v-if="!meals.length" class="flex justify-center text-gray-600">
-    There are no meals
-  </div>
+    <Meals />
 </template>
 
 <script setup>
