876f6083d93b5d65714e8ce7626967fa69bb1f35
DeenoDev <deyanoklaasen@outlook.com>
2024-05-01 23:58:50 +0200

    added empty array to mutations.js when meal variable is null.

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,11 +1,11 @@
 export function setSearchedMeals(state, meals) {
-    state.searchedMeals = meals;
+    state.searchedMeals = meals || [];
 }
 
 export function setMealsByLetter(state, meals) {
-    state.mealsByLetter = meals;
+    state.mealsByLetter = meals || [];
 }
 
 export function setMealsByIngredients(state, meals) {
-    state.mealsByIngredient = meals;
+    state.mealsByIngredient = meals || [];
 }
