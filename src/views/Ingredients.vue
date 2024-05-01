8a13cf9470fb843d91b2d36cb4f28f5e41a8edaa
DeenoDev <deyanoklaasen@outlook.com>
2024-05-02 00:02:52 +0200

    Passed strIngredient into params: ingredient property.

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="p-8">
         <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
-        <router-link :to="{name: 'byIngredient', params: {ingredient: ingredient.idIngredient}}"  v-for="ingredient of ingredients" :key="ingredient.id" 
+        <router-link :to="{name: 'byIngredient', params: {ingredient: ingredient.strIngredient},}"  v-for="ingredient of ingredients" :key="ingredient.id" 
           class="bg-white rounded p-3 mb-3 shadow">
           <h3 class="block text2xl font-bold mb-2"> {{ ingredient.strIngredient }} </h3>
           <p> {{ ingredient.strDescription }} </p>
