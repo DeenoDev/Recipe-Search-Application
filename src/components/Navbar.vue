aa1190ed24325c6a6cda1f31e690c815355bd8a2
DeenoDev <deyanoklaasen@outlook.com>
2024-05-06 23:56:01 +0200

    Navbar.vue component refactored.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,21 +1,30 @@
 <template>
-    <header class="bg-white shadow h-16 flex justify-between items-stretch">
-      <router-link :to="{name: 'home'}" class="inline-flex items-center h-full px-5"> 
-        Home
+  <header class="bg-white shadow h-16 flex justify-between items-stretch">
+    <router-link
+      :to="{ name: 'home' }"
+      class="inline-flex items-center h-full px-5 text-orange-500 font-bold"
+    >
+      Home
+    </router-link>
+    <div class="flex items-center gap-1">
+      <router-link
+        :to="{ name: 'byName' }"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white"
+      >
+        Search Meals
       </router-link>
-      <!-- try using UL tag for this section when tutorial is completed -->
-      <div class="flex items-center gap-1">
-       <router-link :to="{name: 'byName'}" class="inline-flex items center px-2 h-full transition-colors hover:bg-purple-300 pt-5">
-         Search Meals
-         </router-link>
-         <router-link :to="{name: 'byLetter'}" class="inline-flex px-2 center h-full transition-colors hover:bg-purple-300 pt-5">
-          Meals By Letter
-         </router-link>
-         <router-link :to="{name: 'ingredients'}" class="inline-flex px-2 center h-full transition-colors hover:bg-purple-300 pt-5">
-          Meals By Ingredients
-         </router-link>
-
-      </div>
-
-    </header>
+      <router-link
+        :to="{ name: 'byLetter' }"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white"
+      >
+        Meals By Letter
+      </router-link>
+      <router-link
+        :to="{ name: 'ingredients' }"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-orange-500 hover:text-white"
+      >
+        Meals By Ingredients
+      </router-link>
+    </div>
+  </header>
 </template>
