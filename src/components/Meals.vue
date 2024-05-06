47a6402e90477a88fe38973d8057f6c516145e3c
DeenoDev <deyanoklaasen@outlook.com>
2024-05-06 23:53:18 +0200

    Meals.vue component refactored.

--- src/components/Meals.vue
+++ src/components/Meals.vue
@@ -1,9 +1,8 @@
 <template>
-
-<div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
-    <MealItem v-for="meal of meals" :key="meal.idMeal" :meals="meals" />
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 px-8">
+    <MealItem v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
   </div>
-  <div v-if="!meals.length" class="flex justify-center text-gray-600">
+  <div v-if="!meals.length" class="flex justify-center text-gray-600 p-8">
     There are no meals
   </div>
 </template>
@@ -11,11 +10,11 @@
 <script setup>
 import MealItem from './MealItem.vue';
 
- const {meals} = defineProps({
-    meals: {
-        required: true,
-        type: Array,
-    }
- })
+const { meals } = defineProps({
+  meals: {
+    required: true,
+    type: Array,
+  }
+})
 
 </script>
