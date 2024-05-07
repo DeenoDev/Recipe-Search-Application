224270c5f5a8326ac30dadf68e67494e0e5242f4
DeenoDev <deyanoklaasen@outlook.com>
2024-05-08 00:03:37 +0200

    views/Home.vue component refactored.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,23 @@
 <template>
-    <div class="flex flex-col p-8">
-        
-
-        <div class="flex gap-4 justify-center mt-2">
-            <router-link :to='{name: "byLetter", params: { letter }}' v-for="letter of letters" :key ="letter">
-                {{ letter }}
-            </router-link>
-        </div>
-
-        <!-- <pre>{{ ingredients }}</pre> -->
+    <div class="p-8 pb-0 text-orange-500">
+      <h1 class="text-4xl font-bold mb-4">Random Meals</h1>
     </div>
-</template>
-
-<script setup>
-import { computed, onMounted, ref } from 'vue';
-import store from '../store';
-import axiosClient from '../axiosClient.js';
-
-
-
-
-
-const ingredients = ref([]);
-
-onMounted( async() => {
-  const response = await axiosClient.get('/list.php?i=list');
-//   console.log(response.data);
-//   ingredients.value = response.data;
-});
-
-</script>
+    <Meals :meals="meals" />
+  </template>
+  
+  <script setup>
+  import { computed, onMounted, ref } from "vue";
+  import store from "../store";
+  import Meals from "../components/Meals.vue";
+  import axiosClient from "../axiosClient.js";
+  
+  const meals = ref([]);
+  
+  onMounted(async () => {
+    for (let i = 0; i < 10; i++) {
+      axiosClient
+        .get(`random.php`)
+        .then(({ data }) => meals.value.push(data.meals[0]));
+    }
+  });
+  </script>
