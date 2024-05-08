1654d944c001e60400a84c8f4d1fee4d68ae66da
DeenoDev <deyanoklaasen@outlook.com>
2024-05-08 23:50:59 +0200

    axiosClient.js file refactored.

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const axiosClient = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
-  });
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+});
 
-  export default axiosClient;
+
+export default axiosClient;
