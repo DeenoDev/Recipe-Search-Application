be22deea5b641d862299cb9eb9bda1fc189c168e
DeenoDev <deyanoklaasen@outlook.com>
2024-05-08 23:49:51 +0200

    MealsByName.vue component refactored.

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,33 +1,34 @@
 <template>
-    <div class="p-8 pb-0">
-       <input 
-        type="text" 
-        v-model="keyword"
-        class="rounded border-2 bg-white border-gray-200 w-full" 
-        placeholder="Search for Meals"
-        @change="searchMeals()"
-       />   
-    </div>
+  <div class="p-8 pb-0">
+    <h1 class="text-4xl font-bold mb-4 text-orange-500">Search Meals by Name</h1>
+  </div>
+  <div class="px-8 pb-3">
+    <input
+      type="text"
+      v-model="keyword"
+      class="rounded border-2 bg-white border-gray-200 focus:ring-orange-500 focus:border-orange-500 w-full"
+      placeholder="Search for Meals"
+      @change="searchMeals"
+    />
+  </div>
 
-   <Meals :meals="meals" />
-
+  <Meals :meals="meals" />
 </template>
 
 <script setup>
-import {computed} from '@vue/reactivity';
-import { onMounted, ref } from 'vue';
-import store from '../store';
-import Meals from '../components/Meals.vue';
-import { useRoute } from 'vue-router';
+import { computed } from "@vue/reactivity";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+import store from "../store";
+import Meals from '../components/Meals.vue'
 
 const route = useRoute();
-
-const keyword = ref('');
+const keyword = ref("");
 const meals = computed(() => store.state.searchedMeals);
 
-function searchMeals(){
-  if (keyword.value){
-  store.dispatch('searchMeals', keyword.value);
+function searchMeals() {
+  if (keyword.value) {
+    store.dispatch("searchMeals", keyword.value);
   } else {
     store.commit("setSearchedMeals", []);
   }
@@ -36,8 +37,7 @@ function searchMeals(){
 onMounted(() => {
   keyword.value = route.params.name
   if (keyword.value) {
-    searchMeals();
+    searchMeals()
   }
 })
-
 </script>
